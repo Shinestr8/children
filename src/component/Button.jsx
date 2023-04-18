59608c172da4a9ae46a75627e1dc03eaa334701d
Corentin Barbaud <cbarbaud@myeasyfarm.com>
2023-04-18 17:43:57 +0200

    high option

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,3 +1,3 @@
-export const Button = ({onClick, text, red, long}) => {
-    return <button onClick={onClick} style={{backgroundColor: red ? "red" : "", width: long ? "500px" : ""}}>{text}</button>
+export const Button = ({onClick, text, red, long, high}) => {
+    return <button onClick={onClick} style={{backgroundColor: red ? "red" : "", width: long ? "500px" : "", height: "500px"}}>{text}</button>
 }
